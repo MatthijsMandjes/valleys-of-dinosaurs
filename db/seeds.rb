05cb5a7233f1c5b59bee3f7c25df85b9b6428577
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-01 13:25:07 +0200

    Added pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+
+age = rand(1.100)
+dino4 = Dinosaur.create!( { name: "Tyrex", age: age, image_url: "https://goo.gl/aoZsYt"})
