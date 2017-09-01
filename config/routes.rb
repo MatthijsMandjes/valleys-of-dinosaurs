fabefca80910197fe6cb9c3448401253ff0da61a
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-01 16:40:14 +0200

    Edit, update and delete dinosaurs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dinosaurs#index"
+  resources :dinosaurs
+  
+  # get "dinosaurs" => "dinosaurs#index"
+  # get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
+  # get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 
-  get "dinosaurs" => "dinosaurs#index"
-  get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
-  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  # post "dinosaurs" => "dinosaurs#create"
+  # get "dinosaurs/:id/edit" => "dinosaurs#edit", as: :edit_dinosaur
 
-  post "dinosaurs" => "dinosaurs#create"
+  # patch "dinosaurs/:id" => "dinosaurs#update"
+
+  # delete "dinosaurs/:id" => "dinosaurs#destroy"
+
 end
